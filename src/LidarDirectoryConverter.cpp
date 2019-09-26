046d0ee7a8bee6c2824106125663853d33e25cfc
ChernoA <stacker.chau@gmail.com>
2019-09-26 09:15:41 -0400

    Fix reinterpret_cast, field datatype, cloud row_step

--- src/LidarDirectoryConverter.cpp
+++ src/LidarDirectoryConverter.cpp
@@ -7,40 +7,54 @@ using namespace std;
 
 namespace nuscenes2bag {
 
-constexpr uint32_t FLOAT32 = 7;
-
 inline void fillFieldsForPointcloud(std::vector<PointField>& fields) {
     PointField field;
-    field.datatype = FLOAT32;
+    field.datatype = sensor_msgs::PointField::FLOAT32;
     field.offset = 0;
     field.count = 1;
     field.name = std::string("x");
     fields.push_back(field);
 
-    field.datatype = FLOAT32;
+    field.datatype = sensor_msgs::PointField::FLOAT32;
     field.offset = 4;
     field.count = 1;
     field.name = std::string("y");
     fields.push_back(field);
 
-    field.datatype = FLOAT32;
+    field.datatype = sensor_msgs::PointField::FLOAT32;
     field.offset = 8;
     field.count = 1;
     field.name = std::string("z");
     fields.push_back(field);
 
-    field.datatype = FLOAT32;
+    field.datatype = sensor_msgs::PointField::FLOAT32;
     field.offset = 12;
     field.count = 1;
     field.name = std::string("intensity");
     fields.push_back(field);
 }
 
+// Convert float32 to 4 bytes
+union {
+    float value;
+    uint8_t byte[4];
+} floatToBytes;
+
 inline void push_back_float32(std::vector<uint8_t>& data, float float_data) {
+
+  /*
+  // dereferencing type-punned pointer will break strict-aliasing rules
   data.push_back((*reinterpret_cast<uint32_t*>(&float_data) >> 0) & 0xFF);
   data.push_back((*reinterpret_cast<uint32_t*>(&float_data) >> 8) & 0xFF);
   data.push_back((*reinterpret_cast<uint32_t*>(&float_data) >> 16) & 0xFF);
   data.push_back((*reinterpret_cast<uint32_t*>(&float_data) >> 24) & 0xFF);
+  */
+
+  floatToBytes.value = float_data;
+  data.push_back(floatToBytes.byte[0]);
+  data.push_back(floatToBytes.byte[1]);
+  data.push_back(floatToBytes.byte[2]);
+  data.push_back(floatToBytes.byte[3]);
 }
 
 inline std::vector<float> readBinaryPcdFile(std::ifstream& fin) {
@@ -67,8 +81,7 @@ readLidarFile(const std::filesystem::path& filePath)
   PointCloud2 cloud;
   cloud.header.frame_id = std::string("lidar");
   cloud.is_bigendian = false;
-  cloud.point_step = sizeof(uint32_t) * 4;
-  cloud.row_step = 1;
+  cloud.point_step = sizeof(float) * 4; // Length of each point in bytes
   cloud.height = 1;
 
   try {
@@ -88,9 +101,11 @@ readLidarFile(const std::filesystem::path& filePath)
 
     fillFieldsForPointcloud(cloud.fields);
     cloud.data = data;
+    cloud.row_step = data.size(); // Length of row in bytes
 
   } catch (const std::exception& e) {
     PRINT_EXCEPTION(e);
+
     return std::nullopt;
   }
 
